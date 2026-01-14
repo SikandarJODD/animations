a47ebfee1fca87a073a8f7905eaa169c1cc5ab61
SikandarJODD <sikandar6174@gmail.com>
2026-01-14 22:32:10 +0530

    feat: added first component - animated gradient text - lessss gooooo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+  import AnimatedGradientText from "$lib/components/magic-ui/animated-gradient-text/animated-gradient-text.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<div>
+  <AnimatedGradientText class="text-4xl font-semibold">
+    Animated Gradient Text Example
+  </AnimatedGradientText>
+</div>
