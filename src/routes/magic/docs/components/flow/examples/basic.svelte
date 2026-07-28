a01efc29983663e8dd3a6a2a74a81987cd21a6e2
SikandarJODD <sikandar6174@gmail.com>
2026-07-28 06:22:18 +0530

    feat: improved button, copy page dropdown and examples

--- src/routes/magic/docs/components/flow/examples/basic.svelte
+++ src/routes/magic/docs/components/flow/examples/basic.svelte
@@ -5,12 +5,8 @@
 <Flow.Root junctionMarker='none'>
 	<Flow.Node>Start</Flow.Node>
 	<Flow.Parallel>
-		<Flow.List>
 			<Flow.Node>Branch A1</Flow.Node>
 			<Flow.Node>Branch A2</Flow.Node>
-		</Flow.List>
-		<Flow.Node>Branch B</Flow.Node>
-		<Flow.Node>Branch C</Flow.Node>
 	</Flow.Parallel>
 	<Flow.Node>End</Flow.Node>
 </Flow.Root>
--- src/routes/magic/docs/components/flow/examples/vertical.svelte
+++ src/routes/magic/docs/components/flow/examples/vertical.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import * as Flow from '$lib/components/magic/flow';
+	import * as Flow from "$lib/components/magic/flow";
 </script>
 
-<Flow.Root orientation="vertical">
+<Flow.Root orientation="vertical" align="center">
 	<Flow.Node>Step 1</Flow.Node>
 	<Flow.Node>Step 2</Flow.Node>
 	<Flow.Node>Step 3</Flow.Node>
